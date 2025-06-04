e153f7c324a81ea6206352b11c9a841bd10bbb98
wafi <wahfiuddin3173@gmail.com>
2025-06-04 14:46:50 +0700

    add mobile sidebar and prework for navbar. Note: Error occurs double sidebar in desktop view

---
+++ src/app/(dashboard)/_components/navbar-routes.js
@@ -0,0 +1,13 @@
+"use-client";
+
+import { Button } from "@/components/ui/button";
+
+export const NavbarRoutes = () => {
+    return (
+        <div className="flex gap-x-2 ml-auto">
+            <Button>
+                ClickMe!
+            </Button>
+        </div>
+    );
+}
--- src/app/(dashboard)/_components/sidebar-item.js
+++ src/app/(dashboard)/_components/sidebar-item.js
@@ -22,9 +22,9 @@ export const SidebarItem = ({ icon: Icon, label, href }) => {
       onClick={onClick}
       type="button"
       className={cn(
-        "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
+        "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-teal-600 hover:bg-slate-300/20",
         isActive &&
-          "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
+          "text-cyan-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
       )}
     >
       <div className="flex items-center gap-x-2 py-4">
@@ -33,6 +33,12 @@ export const SidebarItem = ({ icon: Icon, label, href }) => {
           className={cn("text-slate-500", isActive && "text-sky-700")}
         />
         {label}
+      </div>
+      <div 
+        className={cn("ml-auto opacity-0 border-2 border-sky-700 h-full transition-all",
+          isActive && "opacity-100")}
+      >
+
       </div>
     </button>
   );
